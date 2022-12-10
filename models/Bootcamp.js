236824d35521cec3f18f6ceb8b8acdc149d27ebd
X-MOD-I <deepanshumodi2000@gmail.com>
2022-12-10 15:04:43 +0530

    added routes and controllers

---
+++ models/Bootcamp.js
@@ -0,0 +1,154 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
+
+const BootcampSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a Name'],
+      unique: true,
+      trim: true,
+      maxlength: [50, 'Name cannot be more than 50 characters'],
+    },
+    slug: String,
+    description: {
+      type: String,
+      required: [true, 'Please add a Description'],
+      maxlength: [500, 'Description cannot be more than 50 characters'],
+    },
+    website: {
+      type: String,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        'Please use a valid url with http/https',
+      ],
+    },
+    phone: {
+      type: String,
+      maxlength: [20, 'Phone number can not be longer than 20 characters'],
+    },
+    email: {
+      type: String,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please add a valid email',
+      ],
+    },
+    address: {
+      type: String,
+      required: [true, 'Please add an address'],
+    },
+    location: {
+      //GeoJson Point
+      type: {
+        type: String,
+        enum: ['Point'],
+      },
+      coordinates: {
+        type: [Number],
+        index: '2dsphere',
+      },
+      formattedAddress: String,
+      street: String,
+      city: String,
+      state: String,
+      zipcode: String,
+      country: String,
+    },
+    careers: {
+      //Array of strings
+      type: [String],
+      required: true,
+      enum: [
+        'Web Development',
+        'Mobile Development',
+        'UI/UX',
+        'Data Science',
+        'Business',
+        'Other',
+      ],
+    },
+    averageRating: {
+      type: Number,
+      min: [1, 'Rating must be atleast 1'],
+      max: [10, 'Rating cannot be more than 10'],
+    },
+    averageCost: Number,
+    photo: {
+      type: String,
+      default: 'no-photo.jpg',
+    },
+    housing: {
+      type: Boolean,
+      default: false,
+    },
+    jobAssistance: {
+      type: Boolean,
+      default: false,
+    },
+    jobGuarantee: {
+      type: Boolean,
+      default: false,
+    },
+    acceptGi: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+//Create bootcamp slug from the name field
+BootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+//Geocode & create location field
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  //Do not save address in DB as we already have one copy in formattedAddress field
+  this.address = undefined;
+  next();
+});
+
+//Cascade delete courses when a respective bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+  console.log(`Courses being removed from a respective bootcamp ${this._id}`);
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
+});
+
+//Reverse populate with virtuals
+BootcampSchema.virtual('courses', {
+  ref: 'Course',
+  localField: '_id',
+  foreignField: 'bootcamp',
+  justOne: false, //to get an array of courses not just one course
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Bootcamp API
+
+>The backend for a bootcamp directory website.
+
+## Install Dependencies
+```
+npm install
+```
+## Run App
+```
+#Run in dev mode
+npm run dev
+
+#Run in prod mode
+
+npm start
+```
+### DOCUMENTATION
+https://documenter.getpostman.com/view/24158006/2s8YzTShAZ
+
+
+
+- Version: 1.0.0
+
+- License: MIT
+
